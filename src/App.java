0d174009b93ac20cb9007f4a37061388613b0e81
Enrique Munoz <esan1211@gmail.com>
2023-01-29 13:18:37 -0700

    Created MinerAir flight menu

---
+++ src/App.java
@@ -0,0 +1,192 @@
+//PA0 Enrique Munoz CS3331
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ArrayList<Flight> Flights = new ArrayList<Flight>(); //ArrayList of flights
+        BufferedWriter buffWriter = new BufferedWriter(new FileWriter("Log.txt")); //Log file to track changes
+        BufferedReader Buffreader = new BufferedReader(new FileReader("FlightSchedulePA0.csv"));
+        String line = Buffreader.readLine();
+        ArrayList<String> lines = new ArrayList<>(); //Contains list of every line in csv
+        while ((line = Buffreader.readLine()) != null){
+            lines.add(line);
+        }
+        for(int i = 0; i < lines.size(); i++){ //Traverses through every line in csv ArrayList and creates a string array to split by commas and add new Flight obj to Flights ArrayList
+            String[] readFlights = lines.get(i).split(",");
+            Flights.add(new Flight(readFlights[0], readFlights[1], readFlights[2], readFlights[3], readFlights[4], readFlights[5], readFlights[6], readFlights[7], Integer.valueOf(readFlights[8]), Integer.valueOf(readFlights[9]), Integer.valueOf(readFlights[10]), readFlights[11], readFlights[12], Integer.valueOf(readFlights[13]), Integer.valueOf(readFlights[14]), Integer.valueOf(readFlights[15]), Integer.valueOf(readFlights[16]), Integer.valueOf(readFlights[17]), Integer.valueOf(readFlights[18]), Integer.valueOf(readFlights[19])));
+        }
+        Buffreader.close();
+        
+        //Clears Log File
+        PrintWriter pWriter = new PrintWriter("Log.txt");
+        pWriter.print("");
+        pWriter.close();
+
+        Scanner scnr = new Scanner(System.in);
+        String input = "";
+        System.out.println("Hello, Welcome to MinerAir! \nPlease input a Flight ID to see more information about this flight.");
+        
+        //User interaction
+        while(!input.equals("end")){
+            input = scnr.nextLine();
+            int currIdx = -1; //Idx of flight that was accessed
+            for(int i = 0; i < Flights.size(); i++){
+                if(Flights.get(i).getID().equals(input)){
+                    currIdx = i;     
+                }
+            }
+            if(currIdx == -1){
+                System.out.println("This is not a valid Flight ID, please try again."); //If ID doesn't exist
+                continue;
+            }
+            while(!input.equals("end")){
+                System.out.println("\nInput the corresponding number if you'd like to access any of of these attributes.");
+                System.out.println("\n1.Show Flight Information \n2.Return Any Specific Attribute About the Flight\n3.Update Flight Attributes");
+                input = scnr.nextLine();
+                while(!input.equals("1") && !input.equals("2") && !input.equals("3") && !input.toLowerCase().equals("end")){
+                    System.out.println("Please input a correct number.");
+                    input = scnr.nextLine();    
+                }
+                if(input.equals("1")){
+                    System.out.println("");
+                    Flights.get(currIdx).printFlight();
+                }
+                if(input.equals("2")){
+                    System.out.println("\nWhich attribute would you like to see?");
+                    System.out.println("1.Origin Airport\n2.Origin Code\n3.Destination Airport\n4.Destination Code\n5.Departure Date\n6.Departure Time\n7.Arrival Time\n8.Arrival Date\n9.Duration\n10.Distance\n11.Time Zone Difference\n12.First Class Price\n13.Business Class Price\n14.Main Cabin Price\n15.First Class Seats\n16.Business Class Seats\n17.Main Cabin Seats\n18.Total Seats");
+                    input = scnr.nextLine();
+                    switch(input){
+                        case "1": 
+                            System.out.println(Flights.get(currIdx).getOriginAirport());
+                            break;
+                        case "2": 
+                            System.out.println(Flights.get(currIdx).getOriginCode());
+                            break;
+                        case "3":
+                            System.out.println(Flights.get(currIdx).getDestinationAirport());
+                            break;
+                        case "4":
+                            System.out.println(Flights.get(currIdx).getDestinationCode());
+                            break;
+                        case "5":
+                            System.out.println(Flights.get(currIdx).getDepartureDate());
+                            break; 
+                        case "6":
+                            System.out.println(Flights.get(currIdx).getDepartureTime());
+                            break;
+                        case "7":
+                            System.out.println(Flights.get(currIdx).getArrivalDate());
+                            break;
+                        case "8":
+                            System.out.println(Flights.get(currIdx).getArrivalTime());
+                            break;          
+                        case "9":
+                            System.out.println(Flights.get(currIdx).getDuration());
+                            break;
+                        case "10":
+                            System.out.println(Flights.get(currIdx).getDistance());
+                            break;
+                        case "11":
+                            System.out.println(Flights.get(currIdx).getTimeZoneDiff());
+                            break;
+                        case "12":
+                            System.out.println(Flights.get(currIdx).getFirstClassPrice());
+                            break;
+                        case "13":
+                            System.out.println(Flights.get(currIdx).getBusinessClassPrice());
+                            break;
+                        case "14":
+                            System.out.println(Flights.get(currIdx).getMainCabinPrice());
+                            break;
+                        case "15":
+                            System.out.println(Flights.get(currIdx).getFirstClassSeats());
+                            break;
+                        case "16":
+                            System.out.println(Flights.get(currIdx).getBusinessSeats());
+                            break;
+                        case "17":
+                            System.out.println(Flights.get(currIdx).getMainCabinSeats());
+                            break;
+                        case "18":
+                            System.out.println(Flights.get(currIdx).getTotalSeats());
+                            break;
+                        default: 
+                            System.out.println("Please input a correct number.");
+                    }
+                }
+                if(input.equals("3")){
+                    System.out.println("\nWhich attribute would you like to change?");
+                    System.out.println("1.Origin Airport\n2.Origin Code\n3.Destination Airport\n4.Destination Code\n5.Departure Date\n6.Departure Time\n7.First Class Price\n8.Business Class Price\n9.Main Cabin Price");
+                    input = scnr.nextLine();
+                    switch(input){
+                        case "1":
+                            System.out.println("What would you like to change the Origin Airport to?");
+                            input = scnr.nextLine();
+                            Flights.get(currIdx).setOriginAirport(input);
+                            buffWriter.write("Flight ID " + Flights.get(currIdx).getID() + " update Origin Airport to " + Flights.get(currIdx).getOriginAirport() +"\n");
+                            break;
+                        case "2": 
+                            System.out.println("What would you like to change the Origin Code to?");
+                            input = scnr.nextLine();
+                            Flights.get(currIdx).setOriginCode(input);
+                            buffWriter.write("Flight ID " + Flights.get(currIdx).getID() + " update Origin Code to " + Flights.get(currIdx).getOriginCode()+"\n");
+                            break;
+                        case "3":
+                            System.out.println("What would you like to change the Destination Airport to?");
+                            input = scnr.nextLine();
+                            Flights.get(currIdx).setDestinationAirport(input);
+                            buffWriter.write("Flight ID " + Flights.get(currIdx).getID() + " update Destination Airport to " + Flights.get(currIdx).getDestinationAirport()+"\n");
+                            break;
+                        case "4":
+                            System.out.println("What would you like to change the Destination Code to?");
+                            input = scnr.nextLine();
+                            Flights.get(currIdx).setDestinationCode(input);
+                            buffWriter.write("Flight ID " + Flights.get(currIdx).getID() + " update Destination Code to " + Flights.get(currIdx).getDestinationCode()+"\n");
+                            break;
+                        case "5":
+                            System.out.println("What would you like to change the Departure Date to?");
+                            input = scnr.nextLine();
+                            Flights.get(currIdx).setDepartureDate(input);
+                            buffWriter.write("Flight ID " + Flights.get(currIdx).getID() + " update Departure Date to " + Flights.get(currIdx).getDepartureDate()+"\n");
+                            break;
+                        case "6":
+                            System.out.println("What would you like to change the Departure Time to?");
+                            input = scnr.nextLine();
+                            Flights.get(currIdx).setDepartureTime(input);
+                            buffWriter.write("Flight ID " + Flights.get(currIdx).getID() + " update Departure Time to " + Flights.get(currIdx).getDepartureTime()+"\n");
+                            break;
+                        case "7":
+                            System.out.println("What would you like to change the First Class Price to?");
+                            input = scnr.nextLine();
+                            Flights.get(currIdx).setFirstClassPrice(Integer.valueOf(input));
+                            buffWriter.write("Flight ID " + Flights.get(currIdx).getID() + " update First Class Price to " + Flights.get(currIdx).getFirstClassPrice()+"\n");
+                            break;
+                        case "8":
+                            System.out.println("What would you like to change the Business Class Price to?");
+                            input = scnr.nextLine();
+                            Flights.get(currIdx).setBusinessClassPrice(Integer.valueOf(input));
+                            buffWriter.write("Flight ID " + Flights.get(currIdx).getID() + " update Business Class Price to " + Flights.get(currIdx).getBusinessClassPrice()+"\n");
+                            break;         
+                        case "9":
+                            System.out.println("What would you like to change the Main Cabin Price to?");
+                            input = scnr.nextLine();
+                            Flights.get(currIdx).setMainCabinPrice(Integer.valueOf(input));
+                            buffWriter.write("Flight ID " + Flights.get(currIdx).getID() + " update Main Cabin Price to " + Flights.get(currIdx).getMainCabinPrice()+"\n");
+                            break;
+                        default:
+                            System.out.println("Please input a correct number.");
+                    }   
+                }
+            }
+            
+        }
+        scnr.close();
+        buffWriter.close();
+
+
+
+        
+    }
+}
---
+++ src/Flight.java
@@ -0,0 +1,277 @@
+public class Flight {
+    //Attributes
+    private String originAirport;
+    private String originCode;
+    private String destinationAirport;
+    private String destinationCode;
+    private String departureDate;
+    private String departureTime;
+    private String arrivalDate;
+    private String arrivalTime;
+    private String ID;
+    private String flightNumber;
+    private int firstClassPrice;
+    private int businessClassPrice;
+    private int mainCabinPrice;
+    private int duration;
+    private int distance;
+    private int timeZoneDiff;
+    private int totalSeats;
+    private int firstClassSeats;
+    private int businessClassSeats;
+    private int mainCabinSeats;
+
+
+    
+    //Constructors
+    public Flight(){
+
+    }
+
+    public Flight(String idIn, String flightNumberIn, String originAirportIn, String originCodeIn, String destinationAirportIn, String destinationCodeIn, String depratingDateIn, String departingTimeIn, int durationIn, int distanceIn, int timeZoneDifferenceIn, String arrivalDateIn, String arrivalTimeIn, int totalSeatsIn, int firstClassSeatsIn, int businessClassSeatsIn, int mainCabinSeatsIn, int firstClassPriceIn, int businessClassPriceIn, int mainCabinPriceIn){
+        this.ID = idIn;
+        this.flightNumber = flightNumberIn;
+        this.originAirport = originAirportIn;
+        this.originCode = originCodeIn;
+        this.destinationAirport = destinationAirportIn;
+        this.destinationCode = destinationCodeIn;
+        this.departureDate = depratingDateIn;
+        this.departureTime = departingTimeIn;
+        this.duration = durationIn;
+        this.distance = distanceIn;
+        this.timeZoneDiff = timeZoneDifferenceIn;
+        this.totalSeats = totalSeatsIn;
+        this.arrivalDate = arrivalDateIn;
+        this.arrivalTime = arrivalTimeIn;
+        this.firstClassSeats = firstClassSeatsIn;
+        this.businessClassSeats = businessClassSeatsIn;
+        this.mainCabinSeats = mainCabinSeatsIn;
+        this.firstClassPrice = firstClassPriceIn;
+        this.businessClassPrice = businessClassPriceIn;
+        this.mainCabinPrice = mainCabinPriceIn;
+
+    }
+
+    //Methods
+    public String[] timeChange(String depTime, int duration){ //Function to update arrival time if destination time is changed
+        String[] result = new String[2]; //Return type in order to return a change of day and also the time change
+        if(depTime == null){ //Need to declare a time first in order to change arrival time
+            result[0] = "0";
+            return result;
+        }
+        int idx = 0;
+        String strHours = "";
+        String strMin = "";
+        String timeOfDay = "";
+        while(depTime.charAt(idx) != ':'){ //Changes hour to int
+            strHours +=  String.valueOf(depTime.charAt(idx));
+            idx+=1;
+        }
+        idx+=1;
+        while(depTime.charAt(idx) != ' '){ //Changes min to int
+            strMin +=  String.valueOf(depTime.charAt(idx));
+            idx+=1;
+        }
+        idx+=1;
+        while(idx < depTime.length()){ //Changes min to int
+            timeOfDay +=  String.valueOf(depTime.charAt(idx));
+            idx+=1;
+        }
+        int intHours = Integer.valueOf(strHours);
+        int intMin = Integer.valueOf(strMin); 
+        while(duration > 0){
+            if(duration < 60){
+                intMin += duration;
+                duration = 0;
+                if(intMin > 59){
+                    intMin -= 60;
+                    intHours += 1;
+                }
+            }else{
+                intHours += 1;
+                duration -= 60;    
+            } 
+        }
+        int daysPassed = 0;
+        if(intHours >= 12){ //Converts AM to PM and vice versa depending on time
+            if(timeOfDay.equals("AM") || strHours.equals("12")){
+                timeOfDay = "PM";
+            }else{
+                timeOfDay = "AM";
+                daysPassed += 1;
+            }    
+        }
+        while(intHours > 12){
+            intHours -= 12;
+        }
+        strHours = String.valueOf(intHours);
+        strMin = String.valueOf(intMin);
+        result[0] = String.valueOf(daysPassed);
+        if (intMin < 10){
+            result[1] = strHours + ":0" + strMin + " " + timeOfDay;    
+        }else{
+            result[1] = strHours + ":" + strMin + " " + timeOfDay;
+        }
+        return result;
+        
+    }
+
+    public String dayChange(String date,int dayChange){ //Changes day if time exceeds 24 hours period
+        String strDay = "";
+        String strMonth = "";
+        String strYear = "";
+        int idx = 0;
+        while(date.charAt(idx)!='/'){
+            strMonth += String.valueOf(date.charAt(idx));
+            idx+=1;
+        }
+        idx+=1;
+        while(date.charAt(idx)!='/'){
+            strDay += String.valueOf(date.charAt(idx));
+            idx+=1;
+        }
+        idx+=1;
+        while(idx < date.length()){
+            strYear += String.valueOf(date.charAt(idx));
+            idx+=1;
+        }
+        int intDay = Integer.valueOf(strDay);
+        int intMonth = Integer.valueOf(strMonth);
+        int intYear = Integer.valueOf(strYear);
+        while(dayChange > 0){
+            intDay += dayChange;
+            if(intDay > 31){
+                intMonth += 1;
+                intDay = 1;
+                if(intMonth > 12){
+                    intMonth = 1;
+                    intYear += 1;
+                }   
+            }
+            dayChange -= 1;
+        }
+        strDay = String.valueOf(intDay);
+        strMonth = String.valueOf(intMonth);
+        strYear = String.valueOf(intYear);
+        return strMonth + "/" + strDay + "/" + strYear;
+        
+    }
+
+    
+    
+    //Setters
+    public void setOriginAirport(String originAirportIn){
+        this.originAirport = originAirportIn;
+    }
+    public void setOriginCode(String originCodeIn){
+        this.originCode = originCodeIn;
+    }
+    public void setDestinationAirport(String destinationAirportIn){
+        this.destinationAirport = destinationAirportIn;
+    }
+    public void setDestinationCode(String destinationCodeIn){
+        this.destinationCode = destinationCodeIn;
+    }
+    public void setDepartureDate(String departureDateIn){
+        this.departureDate = departureDateIn;
+        String[] time = timeChange(this.getDepartureTime(), this.getDuration()); //Changes arrival if departureTime is changed
+        int daysPassed = Integer.valueOf(time[0]);
+        this.arrivalDate = this.dayChange(departureDateIn, daysPassed);
+    }
+    public void setDepartureTime(String departureTimeIn){
+        this.departureTime = departureTimeIn;
+        String[] time = timeChange(departureTimeIn, this.getDuration()); //Changes arrival if departureTime is changed
+        int daysPassed = Integer.valueOf(time[0]);
+        this.arrivalDate = this.dayChange(this.getDepartureDate(), daysPassed);
+        this.arrivalTime = time[1];
+    }
+    public void setFirstClassPrice(int firstClassPriceIn){
+        this.firstClassPrice = firstClassPriceIn;
+    }
+    public void setBusinessClassPrice(int businessClassPriceIn){
+        this.businessClassPrice = businessClassPriceIn;
+    }
+    public void setMainCabinPrice(int mainCabinPriceIn){
+        this.mainCabinPrice = mainCabinPriceIn;
+    }
+
+    //Getters
+    public String getOriginAirport(){
+        return this.originAirport;
+    }
+    public String getOriginCode(){
+        return this.originCode;
+    }
+    public String getDestinationAirport(){
+        return this.destinationAirport;
+    }
+    public String getDestinationCode(){
+        return this.destinationCode;
+    }
+    public String getDepartureDate(){
+        return this.departureDate;
+    }
+    public String getDepartureTime(){
+        return this.departureTime;
+    }
+    public String getArrivalTime(){
+        return this.arrivalTime;
+    }
+    public String getArrivalDate(){
+        return this.arrivalDate;
+    }
+    public int getFirstClassPrice(){
+        return this.firstClassPrice;
+    }
+    public int getBusinessClassPrice(){
+        return this.businessClassPrice;
+    }
+    public int getMainCabinPrice(){
+        return this.mainCabinPrice;
+    }
+    public int getDuration(){
+        return this.duration;
+    }
+    public int getDistance(){
+        return this.distance;
+    }
+    public int getTimeZoneDiff(){
+        return this.timeZoneDiff;
+    }
+    public int getTotalSeats(){
+        return this.totalSeats;
+    }
+    public int getFirstClassSeats(){
+        return this.firstClassPrice;
+    }
+    public int getBusinessSeats(){
+        return this.businessClassPrice;
+    }
+    public int getMainCabinSeats(){
+        return this.mainCabinPrice;
+    }
+    public String getID(){
+        return this.ID;
+    }
+
+    public void printFlight(){ //Prints attributes of obj
+        System.out.println("Origin Airport: " + getOriginAirport());
+        System.out.println("Origin Code: " + getOriginCode());
+        System.out.println("Destination Airport: " + getDestinationAirport());
+        System.out.println("Destination Code: " + getDestinationCode());
+        System.out.println("Departure Date: " + getDepartureDate());
+        System.out.println("Departure Time: " + getDepartureTime());
+        System.out.println("Arrival Date: " + getArrivalDate());
+        System.out.println("Arival Time: " + getArrivalTime());
+        System.out.println("Duration: " + getDuration());
+        System.out.println("Distance: " + getDistance());
+        System.out.println("Time Zone Difference: " + getTimeZoneDiff());
+        System.out.println("First Class Price: " + getFirstClassPrice());
+        System.out.println("Business Class Price: " + getBusinessClassPrice());
+        System.out.println("Main Cabin Price: " + getMainCabinPrice());
+        System.out.println("First Class Seats: " + getFirstClassSeats());
+        System.out.println("Business Class Seats: " + getBusinessSeats());
+        System.out.println("Main Cabin Seats: " + getMainCabinSeats());
+        System.out.println("Total Seats: " + getTotalSeats());
+    }
+}
