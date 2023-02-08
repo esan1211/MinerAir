7144f8afbafa8184ed79c5c9858816093a0b3b06
Enrique Munoz <esan1211@gmail.com>
2023-02-07 23:41:45 -0700

    Reads file and populates Flights ArrayList

--- src/RunFlight.java
+++ src/RunFlight.java
@@ -20,6 +20,7 @@ import java.io.FileNotFoundException;
 public class RunFlight{
     public static void main(String[] args){
         ArrayList<Flight> Flights = new ArrayList<Flight>(); 
+        
         //File Reading
         try{
             BufferedReader buffReader = new BufferedReader(new FileReader("FlightSchedule(1).csv"));
@@ -27,10 +28,20 @@ public class RunFlight{
             ArrayList<String> lines = new ArrayList<>();
             while ((line = buffReader.readLine()) != null){
                 lines.add(line);
+                //System.out.print(line); //Test Print
             }
-            
+            //Populates ArrayList of Flights
+            for(int i = 0; i < lines.size(); i++){ //Traverses through every line in csv ArrayList and creates a string array to split by commas and add new Flight obj to Flights ArrayList
+                String[] readFlights = lines.get(i).split(",");
+                Flights.add(new Flight(readFlights[0], readFlights[1], readFlights[2], readFlights[3], readFlights[4], readFlights[5], readFlights[6], readFlights[7], Integer.valueOf(readFlights[8]), Integer.valueOf(readFlights[9]), Integer.valueOf(readFlights[10]), readFlights[11], readFlights[12], Integer.valueOf(readFlights[13]), Integer.valueOf(readFlights[14]), Integer.valueOf(readFlights[15]), Integer.valueOf(readFlights[16]), Integer.valueOf(readFlights[17]), Integer.valueOf(readFlights[18]), Integer.valueOf(readFlights[19])));
+        }
+            buffReader.close();
         }catch(IOException e){
             System.out.println("The file cannot be found.");
+        }
+
+        for(int i = 0; i < Flights.size(); i++){ //Test to see if flights populated
+            System.out.println(Flights.get(i).getOriginAirport());
         }
     }
 }
