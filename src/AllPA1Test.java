a68a479e1e619c54036b416c7b7fd87aaaa5fd95
Enrique Munoz <esan1211@gmail.com>
2023-02-20 23:56:20 -0700

    Created unit tests and test suite

---
+++ src/AllPA1Test.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+    import org.junit.runners.Suite;
+    import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TestFlightClass.class, TestCustomerClass.class})
+public class AllPA1Test {
+    
+}
